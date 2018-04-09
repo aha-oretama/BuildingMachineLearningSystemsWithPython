cb231cc8d7849c643ae25604438d762ebcf1e30f
aha-oretama <sekine_y_529@msn.com>
2018-04-10 01:52:01 +0900

    Ch1 1.5.3 ndim2

--- ch1/web_traffic.py
+++ ch1/web_traffic.py
@@ -1,6 +1,11 @@
 import scipy as sp
 import matplotlib.pyplot as plt
 
+
+def error(f, x, y):
+    return sp.sum((f(x) - y) ** 2)
+
+
 data = sp.genfromtxt("web_traffic.tsv", delimiter="\t")
 
 print(data[:10])
@@ -11,11 +16,25 @@ y = data[:,1]
 
 print(sp.sum(sp.isnan(y)))
 
+# ndim 1
 fp1, residuals, rank, sv, rcond = sp.polyfit(x,y,1, full=True)
 f1 = sp.poly1d(fp1)
 
 print("Model parameters: %s" % fp1)
 print(residuals)
+
+fx = sp.linspace(0,x[-1],1000)
+plt.plot(fx, f1(fx), linewidth=4)
+
+# ndim 2
+f2p = sp.polyfit(x, y, 2)
+print(f2p)
+f2 = sp.poly1d(f2p)
+print(error(f2,x,y))
+
+plt.plot(fx, f2(fx), linewidth=4)
+
+plt.legend(["d=%i " % w for w in range(1, 3, 1)], loc="upper left")
 
 plt.scatter(x,y)
 plt.title("Web traffic over the last month")
@@ -24,8 +43,4 @@ plt.ylabel("Hits/hour")
 plt.xticks([w*7*24 for w in range(10)],['week %i '%w for w in range(10)])
 plt.autoscale(tight=True)
 plt.grid()
-
-fx = sp.linspace(0,x[-1],1000)
-plt.plot(fx, f1(fx), linewidth=4)
-plt.legend(["d=%i "% f1.order], loc="upper left")
 plt.show()
