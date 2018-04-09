a7161486b7bcfa155fb63b05c129a3717b94a168
aha-oretama <sekine_y_529@msn.com>
2018-04-10 01:36:23 +0900

    Ch1 1.5.3 ndim1

--- ch1/web_traffic.py
+++ ch1/web_traffic.py
@@ -11,6 +11,12 @@ y = data[:,1]
 
 print(sp.sum(sp.isnan(y)))
 
+fp1, residuals, rank, sv, rcond = sp.polyfit(x,y,1, full=True)
+f1 = sp.poly1d(fp1)
+
+print("Model parameters: %s" % fp1)
+print(residuals)
+
 plt.scatter(x,y)
 plt.title("Web traffic over the last month")
 plt.xlabel("Time")
@@ -18,4 +24,8 @@ plt.ylabel("Hits/hour")
 plt.xticks([w*7*24 for w in range(10)],['week %i '%w for w in range(10)])
 plt.autoscale(tight=True)
 plt.grid()
+
+fx = sp.linspace(0,x[-1],1000)
+plt.plot(fx, f1(fx), linewidth=4)
+plt.legend(["d=%i "% f1.order], loc="upper left")
 plt.show()
