a8916058ec98ee1e15679bbcd96fe82b62069bdf
aha-oretama <sekine_y_529@msn.com>
2018-04-14 23:13:19 +0900

    feat: Ch1 1.6 summary

--- ch1/web_traffic.py
+++ ch1/web_traffic.py
@@ -1,3 +1,4 @@
+import os
 import scipy as sp
 import matplotlib.pyplot as plt
 
@@ -6,41 +7,96 @@ def error(f, x, y):
     return sp.sum((f(x) - y) ** 2)
 
 
-data = sp.genfromtxt("web_traffic.tsv", delimiter="\t")
-
+data_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "web_traffic.tsv")
+data = sp.genfromtxt(data_path, delimiter="\t")
 print(data[:10])
-print(data.shape)
+
+# all examples will have three classes in this file
+colors = ['g', 'k', 'b', 'm', 'r']
+linestyles = ['-', '-.', '--', ':', '-']
 
 x = data[:,0]
 y = data[:,1]
+x = x[~sp.isnan(x)]
+y = y[~sp.isnan(y)]
 
-print(sp.sum(sp.isnan(y)))
 
-# ndim 1
-fp1, residuals, rank, sv, rcond = sp.polyfit(x,y,1, full=True)
+# plot
+def plot_models(x, y, models, fname, mx=None, ymax=None, xmin=None):
+    plt.clf()
+    plt.scatter(x, y, s=10)
+    plt.title("Web traffic over the last month")
+    plt.xlabel("Time")
+    plt.ylabel("Hits/hour")
+    plt.xticks([w*7*24 for w in range(10)],['week %i '%w for w in range(10)])
+
+    if models:
+        if mx is None:
+            mx = sp.linspace(0, x[-1], 1000)
+        for model, style, color in zip(models, linestyles, colors):
+            plt.plot(mx, model(mx), linestyle=style, linewidth=2, c=color)
+
+    plt.autoscale(tight=True)
+    plt.ylim(ymin=0)
+    if ymax:
+        plt.ylim(ymax=ymax)
+    if xmin:
+        plt.xlim(xmin=xmin)
+    plt.grid(True, linestyle='-', color='0.75')
+    plt.savefig(fname)
+
+# first look at data
+plot_models(x, y, None, os.path.join(".","1400_01_01.png"))
+
+
+fp1, residuals, rank, sv, rcond = sp.polyfit(x, y, 1, full=True)
 f1 = sp.poly1d(fp1)
+f2 = sp.poly1d(sp.polyfit(x, y, 2))
+f3 = sp.poly1d(sp.polyfit(x, y, 3))
+f10 = sp.poly1d(sp.polyfit(x, y, 10))
+f100 = sp.poly1d(sp.polyfit(x, y, 100))
 
-print("Model parameters: %s" % fp1)
-print(residuals)
+plot_models(x, y, [f1], os.path.join(".", "1400_01_02.png"))
+plot_models(x, y, [f1, f2], os.path.join(".", "1400_01_03.png"))
+plot_models(x, y, [f1, f2, f3, f10, f100], os.path.join(".", "1400_01_04.png"))
 
-fx = sp.linspace(0,x[-1],1000)
-plt.plot(fx, f1(fx), linewidth=4)
 
-# ndim 2
-f2p = sp.polyfit(x, y, 2)
-print(f2p)
-f2 = sp.poly1d(f2p)
-print(error(f2,x,y))
+inflection = int(3.5 * 7 * 24)
+xa = x[:inflection]  # 変化前
+ya = y[:inflection]
+xb = x[inflection:]  # 変化後
+yb = y[inflection:]
 
-plt.plot(fx, f2(fx), linewidth=4)
+fa = sp.poly1d(sp.polyfit(xa, ya, 1))
+fb = sp.poly1d(sp.polyfit(xb, yb, 1))
 
-plt.legend(["d=%i " % w for w in range(1, 3, 1)], loc="upper left")
+plot_models(x, y, [fa, fb], os.path.join(".", "1400_01_05.png"))
 
-plt.scatter(x,y)
-plt.title("Web traffic over the last month")
-plt.xlabel("Time")
-plt.ylabel("Hits/hour")
-plt.xticks([w*7*24 for w in range(10)],['week %i '%w for w in range(10)])
-plt.autoscale(tight=True)
-plt.grid()
-plt.show()
+# extrapolating into the future
+plot_models(
+    x, y, [f1, f2, f3, f10, f100],
+    os.path.join(".", "1400_01_06.png"),
+    mx=sp.linspace(0 * 7 * 24, 6 * 7 * 24, 100),
+    ymax=10000, xmin=0 * 7 * 24)
+
+# separating training from testing data
+frac = 0.3
+split_idx = int(frac * len(xb))
+shuffled = sp.random.permutation(list(range(len(xb))))
+test = sorted(shuffled[:split_idx])
+train = sorted(shuffled[split_idx:])
+
+fbt1 = sp.poly1d(sp.polyfit(xb[train], yb[train], 1))
+fbt2 = sp.poly1d(sp.polyfit(xb[train], yb[train], 2))
+fbt3 = sp.poly1d(sp.polyfit(xb[train], yb[train], 3))
+fbt10 = sp.poly1d(sp.polyfit(xb[train], yb[train], 10))
+fbt100 = sp.poly1d(sp.polyfit(xb[train], yb[train], 100))
+
+for f in [fbt1, fbt2, fbt3, fbt10, fbt100]:
+    print("Error d=%i: %f" % (f.order, error(f, xb[test], yb[test])))
+
+from scipy.optimize import fsolve
+print(fbt2)
+print(fbt2 - 100000)
+reached_max = fsolve(fbt2 - 100000, x0=800) / (7 * 24)
+print("100,000 hits/hour expected at week %f" % reached_max[0])
