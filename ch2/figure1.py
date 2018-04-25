e567bb261669e5680a2f61a283fc347a8d8266df
aha-oretama <sekine_y_529@msn.com>
2018-04-26 01:41:00 +0900

    feat: Ch2 2.1.2

--- ch2/figure1.py
+++ ch2/figure1.py
@@ -7,10 +7,12 @@ features = data['data']
 
 print(features)
 feature_names = data['feature_names']
+print(feature_names)
 target = data['target']
 print(target)
 target_names = data['target_names']
 labels = target_names[target]
+print(labels)
 
 fig,axes = plt.subplots(2,3)
 pairs = [(0, 1), (0, 2), (0, 3), (1, 2), (1, 3), (2, 3)]
@@ -36,3 +38,36 @@ for i, (p0,p1) in enumerate(pairs):
     ax.set_yticks([])
 fig.tight_layout()
 fig.savefig('figure1.png')
+
+# Distinct setosa
+plength = features[:, 2]
+is_setosa = (labels == "setosa")
+
+max_setosa = plength[is_setosa].max()
+min_non_setosa = plength[~is_setosa].min()
+print('Maximum of setosa: {0}.'.format(max_setosa))
+print('Minimum of others: {0}.'.format(min_non_setosa))
+
+features = features[~is_setosa]
+labels = labels[~is_setosa]
+is_virginica = (labels == 'virginica')
+
+## 1D
+best_acc = -1.0
+best_fi = -1.0
+best_t = -1.0
+
+for fi in range(features.shape[1]):
+    thresh = features[:,fi]
+    for t in thresh:
+        feature_i = features[:,fi]
+        pred = (feature_i > t)
+        acc = (pred == is_virginica).mean()
+        if acc > best_acc:
+            best_acc = acc
+            best_fi = fi
+            best_t = t
+
+print("best_acc:{0}".format(best_acc))
+print("best_fi:{0}".format(best_fi))
+print("best_t:{0}".format(best_t))
