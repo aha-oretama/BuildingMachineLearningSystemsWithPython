246cdf97319fa02c56cc2c491a6b4b288b117dcc
aha-oretama <sekine_y_529@msn.com>
2018-04-27 18:07:37 +0900

    feat: Ch3 3.2.3

--- ch3/imaging-databases.py
+++ ch3/imaging-databases.py
@@ -24,6 +24,13 @@ def dist_raw(v1, v2):
     delta = v1 - v2
     return sp.linalg.norm(delta.toarray())
 
+def dist_norm(v1, v2):
+    v1_normalized = v1/sp.linalg.norm(v1.toarray())
+    v2_normalized = v2/sp.linalg.norm(v2.toarray())
+    delta = v1_normalized - v2_normalized
+    return sp.linalg.norm(delta.toarray())
+
+
 best_doc = None
 best_dist = sys.maxsize
 best_i = None
@@ -33,7 +40,7 @@ for i in range(0, n_samples):
     if post == new_post:
         continue
     post_vec = X_train.getrow(i)
-    d = dist_raw(post_vec, new_post_vec)
+    d = dist_norm(post_vec, new_post_vec)
     print("=== Post %i with dist=%.2f: %s" % (i, d, post))
     if(d < best_dist):
         best_dist = d
